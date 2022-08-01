cd2248f80464a360d24d7674d2c3f321ddc825ab
chuong0608 <chuongtep113@gmail.com>
2022-08-01 17:48:20 +0700

    1-8 chuong git gửi yêu cầu xác nhận xóa bạn

--- src/main/java/com/example/socialnetworkbe/controller/RelationshipController.java
+++ src/main/java/com/example/socialnetworkbe/controller/RelationshipController.java
@@ -4,8 +4,11 @@ import com.example.socialnetworkbe.model.Relationship;
 
 import com.example.socialnetworkbe.service.impl.RelationshipServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @RestController
@@ -16,10 +19,36 @@ public class RelationshipController {
     @Autowired
     RelationshipServiceImpl relationshipService;
 
-//    @PostMapping()
-//    public ResponseEntity<Relationship> makeFriend(@RequestBody Relationship relationship){
-//
-//
-//
-//    }
+    @PostMapping()
+    public ResponseEntity<Relationship> makeFriend(@RequestBody Relationship relationship){
+        relationshipService.save(relationship);
+        return new ResponseEntity<>(relationship,HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Relationship> updateUserProfile(@PathVariable Long id) {
+        Relationship relationship= new Relationship();
+        Optional<Relationship> relationshipOptional = this.relationshipService.findById(id);
+        if (!relationshipOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        relationship.setId(relationshipOptional.get().getId());
+        relationship.setUser1(relationshipOptional.get().getUser1());
+        relationship.setUser2(relationshipOptional.get().getUser2());
+        relationship.setStatus(2);
+        relationshipService.save(relationship);
+        return new ResponseEntity<>(relationship, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        Optional<Relationship> relationshipOptional = relationshipService.findById(id);
+        if(!relationshipOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        relationshipService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
