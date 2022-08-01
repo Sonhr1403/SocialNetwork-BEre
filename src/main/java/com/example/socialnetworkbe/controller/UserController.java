1c04e877daeef41accc81901c201e3d1efe83878
chuong0608 <chuongtep113@gmail.com>
2022-08-01 09:11:40 +0700

    1-8 chuong sua update-profile

--- src/main/java/com/example/socialnetworkbe/controller/UserController.java
+++ src/main/java/com/example/socialnetworkbe/controller/UserController.java
@@ -137,6 +137,7 @@ public class UserController {
         user.setPassword(userOptional.get().getPassword());
         user.setRoles(userOptional.get().getRoles());
         user.setConfirmPassword(userOptional.get().getConfirmPassword());
+        user.setAvatar(userOptional.get().getAvatar());
         userService.save(user);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
