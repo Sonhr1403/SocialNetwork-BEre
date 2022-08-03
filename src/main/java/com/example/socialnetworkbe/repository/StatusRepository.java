bd8a5ef43a1bf5919e343cc70b07d4d40c509d46
VietHoang319 <arishoang319@gmail.com>
2022-08-03 22:29:59 +0700

    3/8 - Hoàng - sửa query sắp xếp bài đăng của cá nhân theo ngày

--- src/main/java/com/example/socialnetworkbe/repository/StatusRepository.java
+++ src/main/java/com/example/socialnetworkbe/repository/StatusRepository.java
@@ -11,6 +11,6 @@ public interface StatusRepository extends JpaRepository<Status, Long> {
     @Query(value = "select * from status order by id desc limit 1", nativeQuery = true)
     Status findLastStatus();
 
-    @Query(value = "select * from status where owner_id = :id and status <> 0", nativeQuery = true)
+    @Query(value = "select * from status where owner_id = :id and status <> 0 order by create_at desc", nativeQuery = true)
     Iterable<Status> findAllByOwner(@Param("id") Long id);
 }
