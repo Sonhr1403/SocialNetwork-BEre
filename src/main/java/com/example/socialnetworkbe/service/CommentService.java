02f470b128f46dc29f6f17cf6f9ff2f3f97c0ea0
chandoithatsu <100838893+Afdfghjhgcfghvh@users.noreply.github.com>
2022-08-05 08:05:24 +0700

    non

--- src/main/java/com/example/socialnetworkbe/service/CommentService.java
+++ src/main/java/com/example/socialnetworkbe/service/CommentService.java
@@ -1,4 +1,14 @@
 package com.example.socialnetworkbe.service;
 
-public interface CommentService {
+import java.util.Optional;
+
+public interface CommentService <T>{
+    Iterable<T> findAll();
+
+    Optional<T> findById(Long id);
+
+    void save(T t);
+
+    void remove(Long id);
+
 }
--- src/main/java/com/example/socialnetworkbe/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/socialnetworkbe/service/impl/CommentServiceImpl.java
@@ -1,11 +1,37 @@
 package com.example.socialnetworkbe.service.impl;
 
+import com.example.socialnetworkbe.model.Comment;
+import com.example.socialnetworkbe.model.Image;
 import com.example.socialnetworkbe.repository.CommentRepository;
+import com.example.socialnetworkbe.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class CommentServiceImpl {
+public class CommentServiceImpl implements CommentService<Comment> {
     @Autowired
     private CommentRepository commentRepository;
+
+
+    @Override
+    public Iterable<Comment> findAll() {
+        return commentRepository.findAll();
+    }
+
+    @Override
+    public Optional<Comment> findById(Long id) {
+        return commentRepository.findById(id);
+    }
+
+    @Override
+    public void save(Comment comment) {
+        commentRepository.save(comment);
+    }
+
+    @Override
+    public void remove(Long id) {
+        commentRepository.deleteById(id);
+    }
 }
