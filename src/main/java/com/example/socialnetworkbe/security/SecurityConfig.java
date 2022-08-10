cb0f18253bc151c3b61054409fabfd56aa6071e1
VietHoang319 <arishoang319@gmail.com>
2022-08-10 17:13:45 +0700

    10/8 - Hoàng - security

--- src/main/java/com/example/socialnetworkbe/security/SecurityConfig.java
+++ src/main/java/com/example/socialnetworkbe/security/SecurityConfig.java
@@ -72,9 +72,8 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
         http.csrf().ignoringAntMatchers("/**");
         http.httpBasic().authenticationEntryPoint(restServicesEntryPoint());
         http.authorizeRequests()
-                .antMatchers("/**").permitAll()
-//                .antMatchers("/login", "/register", "/hello").permitAll()
-//                .antMatchers("/users/**").access("hasRole('ROLE_USER')")
+                .antMatchers("/login", "/register").permitAll()
+                .antMatchers("/users/**", "/comments/**", "/images/**", "/like-comments/**", "/like-statuses/**", "/relationships/**", "/statuses/**").access("hasRole('ROLE_USER')")
 //                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
 //                .antMatchers(HttpMethod.GET
 //                        ).access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
